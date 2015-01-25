2bffa4305b710c0d9a082abe565097146677cbe2
Cordon Davies <cordondavies@Cordons-MacBook-Pro.local>
2015-01-25 01:14:57 -0700

    build

--- head.php
+++ head.php
@@ -1,4 +1,4 @@
-<head>
+<head> 
 	<title>Messenger</title>
 	<link rel="stylesheet" type="text/css" href="/bower_components/bootstrap/dist/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
